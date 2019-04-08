07c690f03d9091a1f2551a13e2d54ca3342f7340
DaehunGwak <pmon2648@daum.net>
2019-04-08 19:37:29 +0900

    done data transmission android to flask using params 데이터 주고 받기를 body가 아닌 url params로 해결

--- Server/main.py
+++ Server/main.py
@@ -2,9 +2,12 @@
 
 # 포스트 예제
 
-from flask import Flask
+from flask import Flask, make_response
+from flask import request
 from flask_restful import Resource, Api
 from flask_restful import reqparse
+from pprint import pprint 
+import json
 
 app = Flask(__name__)
 api = Api(app)
@@ -13,27 +16,30 @@ class CreateUser(Resource):
     def post(self):
         try:
             parser = reqparse.RequestParser()
-            parser.add_argument('email', type=str)
-            parser.add_argument('user_name', type=str)
-            parser.add_argument('password', type=str)
+            parser.add_argument('data', type=str)
             args = parser.parse_args()
 
-            _userEmail = args['email']
-            _userName = args['user_name']
-            _userPassword = args['password']
+            _userData = json.loads(args['data'])
 
+            # test logs
+            pprint(args)
+            pprint(type(_userData))
+            pprint(_userData)
+
+            return {
+                "status": "success"
+            }
+            '''
             return {
                 'Email': _userEmail,
                 'UserName': _userName,
                 'Password': _userPassword
             }
-
+            '''
         except Exception as e:
             return {'error': str(e)}
-        
 
-api.add_resource(CreateUser, '/user')
-
+api.add_resource(CreateUser, '/')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
